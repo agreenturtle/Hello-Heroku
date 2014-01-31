a463723673d51767ad819438c8473c8df5748a63
agreenturtle <james.lai.chen@gmail.com>
2014-01-30 22:12:37 -0800

    init

--- server.js
+++ server.js
@@ -19,10 +19,12 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+app.set('view engine', 'jade');
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render("application");
+  //res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
